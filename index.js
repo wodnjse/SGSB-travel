5cebd1a04f438ff326cc7529f9f1c64f726f09c6
wodnjse <macimaro04@gmail.com>
2022-11-28 20:56:39 +0900

    Fix: 여행 리스트 'undefine' 출력 오류 수정

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-
-function Seoul() { 
+function Seoul() {
     fetch('http://localhost:8000/data/seoul') // 해당 주소의 api를 불러옴
         .then((response) => response.json()) // 불러온 api 값을 response로 받고 response값을 json으로 리턴 한다.
         .then((data) => { //그 값이 data라는 변수에 json array 형태로 들어감
             data.forEach(element => { //가져온 값의 모든 값을 가져오기 위해 forEach문으로 모든 요소를 반복해 가져옴
                 const list = document.getElementById('listElement') // 읽어온 데이터를 추가할 리스트의 아이디를 가져옴
-                list.insertAdjacentHTML('beforeend', `            
-            <li class = "auctionRow" onclick = "location.href='https://www.mangoplate.com/search/${element.명소}'">
+                list.insertAdjacentHTML('beforeend', `
+                <li class = "auctionRow" onclick = "location.href='https://www.mangoplate.com/search/${element.명소}'">
             <p class = "auctionID">명소: ${element.명소}</p>
                         <p class = "auctionAddress">별점: ${element.별점}</p>
                         </li>
@@ -15,14 +14,13 @@ function Seoul() {
         });
 }
 
-
-function Gyeonggi(){
+function Gyeonggi() {
     fetch('http://localhost:8000/data/gyeonggi') // 해당 주소의 api를 불러옴
         .then((response) => response.json()) // 불러온 api 값을 response로 받고 response값을 json으로 리턴 한다.
         .then((data) => { //그 값이 data라는 변수에 json array 형태로 들어감
             data.forEach(element => { //가져온 값의 모든 값을 가져오기 위해 forEach문으로 모든 요소를 반복해 가져옴
                 const list = document.getElementById('listElement') // 읽어온 데이터를 추가할 리스트의 아이디를 가져옴
-                list.insertAdjacentHTML('beforeend',`            
+                list.insertAdjacentHTML('beforeend', `
                 <li class = "auctionRow" onclick = "location.href='https://www.mangoplate.com/search/${element.명소}'">
                         <p class = "auctionID">명소: ${element.명소}</p>
                         <p class = "auctionAddress">별점: ${element.별점}</p>
@@ -32,14 +30,13 @@ function Gyeonggi(){
         });
 }
 
-
-function Sokcho(){
+function Sokcho() {
     fetch('http://localhost:8000/data/sokcho') // 해당 주소의 api를 불러옴
         .then((response) => response.json()) // 불러온 api 값을 response로 받고 response값을 json으로 리턴 한다.
         .then((data) => { //그 값이 data라는 변수에 json array 형태로 들어감
             data.forEach(element => { //가져온 값의 모든 값을 가져오기 위해 forEach문으로 모든 요소를 반복해 가져옴
                 const list = document.getElementById('listElement') // 읽어온 데이터를 추가할 리스트의 아이디를 가져옴
-                list.insertAdjacentHTML('beforeend',`            
+                list.insertAdjacentHTML('beforeend', `
                 <li class = "auctionRow" onclick = "location.href='https://www.mangoplate.com/search/${element.명소}'">
                         <p class = "auctionID">명소: ${element.명소}</p>
                         <p class = "auctionAddress">별점: ${element.별점}</p>
@@ -51,36 +48,40 @@ function Sokcho(){
 
 }
 
-
-function Busan(){
+function Busan() {
     fetch('http://localhost:8000/data/busan') // 해당 주소의 api를 불러옴
         .then((response) => response.json()) // 불러온 api 값을 response로 받고 response값을 json으로 리턴 한다.
         .then((data) => { //그 값이 data라는 변수에 json array 형태로 들어감
             data.forEach(element => { //가져온 값의 모든 값을 가져오기 위해 forEach문으로 모든 요소를 반복해 가져옴
                 const list = document.getElementById('listElement') // 읽어온 데이터를 추가할 리스트의 아이디를 가져옴
-                list.insertAdjacentHTML('beforeend',`            
+                list.insertAdjacentHTML('beforeend', `
                 <li class = "auctionRow" onclick = "location.href='https://www.mangoplate.com/search/${element.명소}'">
                         <p class = "auctionID">명소: ${element.명소}</p>
                         <p class = "auctionAddress">별점: ${element.별점}</p>
-                    </li>
+                        </li>
                 `); // 아이디로 가져온 값을 html 문으로 반복해서 뒤에 추가함
             });
         });
-} 
-
-
-function loadSeoul(){
-        document.getElementById('listElement').innerHTML = Seoul();
 }
 
-function loadGyeonggi(){
-    document.getElementById('listElement').innerHTML = Gyeonggi();
+const a = getElementById('seoul')
+
+function loadSeoul() {
+    document.getElementById('listElement').innerHTML = '';
+    Seoul();
 }
 
-function loadSokcho(){
-    document.getElementById('listElement').innerHTML = Sokcho();
+function loadGyeonggi() {
+    document.getElementById('listElement').innerHTML = '';
+    Gyeonggi();
 }
 
-function loadBusan(){
-    document.getElementById('listElement').innerHTML = Busan();
+function loadSokcho() {
+    document.getElementById('listElement').innerHTML = '';
+    Sokcho();
+}
+
+function loadBusan() {
+    document.getElementById('listElement').innerHTML = '';
+    Busan();
 }
