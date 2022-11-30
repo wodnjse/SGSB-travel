75d09f8f4299506a5c1794c5783fa48d573d615a
wodnjse <macimaro04@gmail.com>
2022-11-30 20:46:41 +0900

    Refactor: 리스트 '명소', '별점' 텍스트 삭제

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ function Seoul() {
             data.forEach(element => { //가져온 값의 모든 값을 가져오기 위해 forEach문으로 모든 요소를 반복해 가져옴
                 const list = document.getElementById('listElement') // 읽어온 데이터를 추가할 리스트의 아이디를 가져옴
                 list.insertAdjacentHTML('beforeend', `
-                <li class = "auctionRow" onclick = "location.href='https://www.mangoplate.com/search/${element.명소}'">
-            <p class = "auctionID">명소: ${element.명소}</p>
-                        <p class = "auctionAddress">별점: ${element.별점}</p>
+                <li class = "auction-row" onclick = "location.href='https://www.mangoplate.com/search/${element.명소}'">
+            <p class = "auctionID">${element.명소}</p>
+                        <p class = "auctionAddress">${element.별점}</p>
                         </li>
                         `); // 아이디로 가져온 값을 html 문으로 반복해서 뒤에 추가함
             });
@@ -21,9 +21,9 @@ function Gyeonggi() {
             data.forEach(element => { //가져온 값의 모든 값을 가져오기 위해 forEach문으로 모든 요소를 반복해 가져옴
                 const list = document.getElementById('listElement') // 읽어온 데이터를 추가할 리스트의 아이디를 가져옴
                 list.insertAdjacentHTML('beforeend', `
-                <li class = "auctionRow" onclick = "location.href='https://www.mangoplate.com/search/${element.명소}'">
-                        <p class = "auctionID">명소: ${element.명소}</p>
-                        <p class = "auctionAddress">별점: ${element.별점}</p>
+                <li class = "auction-row" onclick = "location.href='https://www.mangoplate.com/search/${element.명소}'">
+                        <p class = "auctionID">${element.명소}</p>
+                        <p class = "auctionAddress">${element.별점}</p>
                     </li>
                 `); // 아이디로 가져온 값을 html 문으로 반복해서 뒤에 추가함
             });
@@ -37,9 +37,9 @@ function Sokcho() {
             data.forEach(element => { //가져온 값의 모든 값을 가져오기 위해 forEach문으로 모든 요소를 반복해 가져옴
                 const list = document.getElementById('listElement') // 읽어온 데이터를 추가할 리스트의 아이디를 가져옴
                 list.insertAdjacentHTML('beforeend', `
-                <li class = "auctionRow" onclick = "location.href='https://www.mangoplate.com/search/${element.명소}'">
-                        <p class = "auctionID">명소: ${element.명소}</p>
-                        <p class = "auctionAddress">별점: ${element.별점}</p>
+                <li class = "auction-row" onclick = "location.href='https://www.mangoplate.com/search/${element.명소}'">
+                        <p class = "auctionID">${element.명소}</p>
+                        <p class = "auctionAddress">${element.별점}</p>
                     </li>
                 `); // 아이디로 가져온 값을 html 문으로 반복해서 뒤에 추가함
             });
@@ -55,16 +55,16 @@ function Busan() {
             data.forEach(element => { //가져온 값의 모든 값을 가져오기 위해 forEach문으로 모든 요소를 반복해 가져옴
                 const list = document.getElementById('listElement') // 읽어온 데이터를 추가할 리스트의 아이디를 가져옴
                 list.insertAdjacentHTML('beforeend', `
-                <li class = "auctionRow" onclick = "location.href='https://www.mangoplate.com/search/${element.명소}'">
-                        <p class = "auctionID">명소: ${element.명소}</p>
-                        <p class = "auctionAddress">별점: ${element.별점}</p>
+                <li class = "auction-row" onclick = "location.href='https://www.mangoplate.com/search/${element.명소}'">
+                        <p class = "auctionID">${element.명소}</p>
+                        <p class = "auctionAddress">${element.별점}</p>
                         </li>
                 `); // 아이디로 가져온 값을 html 문으로 반복해서 뒤에 추가함
             });
         });
 }
 
-const a = getElementById('seoul')
+// const a = getElementById('seoul')
 
 function loadSeoul() {
     document.getElementById('listElement').innerHTML = '';
