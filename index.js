2e40f72bc15e27a94a78bc96db7f0b77c3bc8893
wodnjse <macimaro04@gmail.com>
2022-12-01 21:44:58 +0900

    Refactor: Tag 코드 refactoring

--- index.js
+++ index.js
@@ -61,28 +61,6 @@ function Busan() {
             });
         });
 }
-function getCityData() {
-    fetch('http://localhost:8000/data/url) // 해당 주소의 api를 불러옴
-        .then((response) => response.json()) // 불러온 api 값을 response로 받고 response값을 json으로 리턴 한다.
-        .then((data) => { //그 값이 data라는 변수에 json array 형태로 들어감
-            data.forEach(element => { //가져온 값의 모든 값을 가져오기 위해 forEach문으로 모든 요소를 반복해 가져옴
-                const list = document.getElementById('listElement') // 읽어온 데이터를 추가할 리스트의 아이디를 가져옴
-                list.insertAdjacentHTML('beforeend', `
-                <li class = "auction-row" onclick = "window.open('https://www.mangoplate.com/search/${element.명소}')">
-                        <p class = "auctionID">${element.명소}</p>
-                        <p class = "auctionAddress">⭐${element.별점}</p>
-                        </li>
-                `); // 아이디로 가져온 값을 html 문으로 반복해서 뒤에 추가함
-            });
-        });
-
-}
-
-
-function loadCity(url) {
-    document.getElementById('listElement').innerHTML = '';
-    getCityData(url);
-}
 
 function changeTagSeoul() {
     document.getElementById('tagSeoul').style.backgroundColor = "#186ADE";
@@ -117,6 +95,38 @@ function changeTagSockcho() {
     document.getElementById('tagGyeonggi').style.color= "#000";
     document.getElementById('tagBusan').style.color= "#000";
 }
+function getCityData(city) {
+    fetch(`http://localhost:8000/data/${city}`) // 해당 주소의 api를 불러옴
+        .then((response) => response.json()) // 불러온 api 값을 response로 받고 response값을 json으로 리턴 한다.
+        .then((data) => { //그 값이 data라는 변수에 json array 형태로 들어감
+            data.forEach(element => { //가져온 값의 모든 값을 가져오기 위해 forEach문으로 모든 요소를 반복해 가져옴
+                const list = document.getElementById('listElement') // 읽어온 데이터를 추가할 리스트의 아이디를 가져옴
+                list.insertAdjacentHTML('beforeend', `
+                <li class = "auction-row" onclick = "window.open('https://www.mangoplate.com/search/${element.명소}')">
+                        <p class = "auctionID">${element.명소}</p>
+                        <p class = "auctionAddress">⭐${element.별점}</p>
+                        </li>
+                `); // 아이디로 가져온 값을 html 문으로 반복해서 뒤에 추가함
+            });
+        });
+
+}
+function loadCity(city) {
+    let tags = {
+        'seoul': 'tagSeoul',
+        'gyeonggi': 'tagGyeonggi',
+        'sokcho': 'tagSokcho',
+        'busan': 'tagBusan'
+    }
+
+    document.getElementById('listElement').innerHTML = '';
+    getCityData(city);
+    changeTag(tags[city]);
+}
+
+
+
+loadCity('seoul');
 
 function changeTag(selectedTag) {
     let tagList = ['tagSeoul', 'tagGyeonggi', 'tagSokcho', 'tagBusan'];
@@ -129,12 +139,12 @@ function changeTag(selectedTag) {
         document.getElementById(tag).style.backgroundColor = "#DCE3E8";
         document.getElementById(tag).style.color = "#000";
     }
-  /*  document.getElementById('tagSeoul').style.backgroundColor= "#DCE3E8";
-    document.getElementById('tagGyeonggi').style.backgroundColor= "#DCE3E8";
-    document.getElementById('tagSokcho').style.backgroundColor= "#DCE3E8";
-    document.getElementById('tagSeoul').style.color= "#000";
-    document.getElementById('tagGyeonggi').style.color= "#000";
-    document.getElementById('tagSokcho').style.color= "#000";*/
+    /*  document.getElementById('tagSeoul').style.backgroundColor= "#DCE3E8";
+      document.getElementById('tagGyeonggi').style.backgroundColor= "#DCE3E8";
+      document.getElementById('tagSokcho').style.backgroundColor= "#DCE3E8";
+      document.getElementById('tagSeoul').style.color= "#000";
+      document.getElementById('tagGyeonggi').style.color= "#000";
+      document.getElementById('tagSokcho').style.color= "#000";*/
 }
 
 
