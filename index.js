472ebe1c0eb8690c9ce9785c681c4007dd9f73ec
wodnjse <macimaro04@gmail.com>
2022-12-01 21:41:45 +0900

    Feat: Tag 기능 추가

--- index.js
+++ index.js
@@ -61,25 +61,97 @@ function Busan() {
             });
         });
 }
+function getCityData() {
+    fetch('http://localhost:8000/data/url) // 해당 주소의 api를 불러옴
+        .then((response) => response.json()) // 불러온 api 값을 response로 받고 response값을 json으로 리턴 한다.
+        .then((data) => { //그 값이 data라는 변수에 json array 형태로 들어감
+            data.forEach(element => { //가져온 값의 모든 값을 가져오기 위해 forEach문으로 모든 요소를 반복해 가져옴
+                const list = document.getElementById('listElement') // 읽어온 데이터를 추가할 리스트의 아이디를 가져옴
+                list.insertAdjacentHTML('beforeend', `
+                <li class = "auction-row" onclick = "window.open('https://www.mangoplate.com/search/${element.명소}')">
+                        <p class = "auctionID">${element.명소}</p>
+                        <p class = "auctionAddress">⭐${element.별점}</p>
+                        </li>
+                `); // 아이디로 가져온 값을 html 문으로 반복해서 뒤에 추가함
+            });
+        });
 
-// const a = getElementById('seoul')
-
-function loadSeoul() {
-    document.getElementById('listElement').innerHTML = '';
-    Seoul();
 }
 
-function loadGyeonggi() {
+
+function loadCity(url) {
     document.getElementById('listElement').innerHTML = '';
-    Gyeonggi();
+    getCityData(url);
 }
 
-function loadSokcho() {
-    document.getElementById('listElement').innerHTML = '';
-    Sokcho();
+function changeTagSeoul() {
+    document.getElementById('tagSeoul').style.backgroundColor = "#186ADE";
+    document.getElementById('tagSeoul').style.color= "#fff";
+
+    document.getElementById('tagGyeonggi').style.backgroundColor= "#DCE3E8";
+    document.getElementById('tagSokcho').style.backgroundColor= "#DCE3E8";
+    document.getElementById('tagBusan').style.backgroundColor= "#DCE3E8";
+    document.getElementById('tagGyeonggi').style.color= "#000";
+    document.getElementById('tagSokcho').style.color= "#000";
+    document.getElementById('tagBusan').style.color= "#000";
+}
+function changeTagGyeonggi() {
+    document.getElementById('tagGyeonggi').style.backgroundColor = "#186ADE";
+    document.getElementById('tagGyeonggi').style.color= "#fff";
+
+    document.getElementById('tagSeoul').style.backgroundColor= "#DCE3E8";
+    document.getElementById('tagSokcho').style.backgroundColor= "#DCE3E8";
+    document.getElementById('tagBusan').style.backgroundColor= "#DCE3E8";
+    document.getElementById('tagSeoul').style.color= "#000";
+    document.getElementById('tagSokcho').style.color= "#000";
+    document.getElementById('tagBusan').style.color= "#000";
+}
+function changeTagSockcho() {
+    document.getElementById('tagSokcho').style.backgroundColor = "#186ADE";
+    document.getElementById('tagSokcho').style.color= "#fff";
+
+    document.getElementById('tagSeoul').style.backgroundColor= "#DCE3E8";
+    document.getElementById('tagGyeonggi').style.backgroundColor= "#DCE3E8";
+    document.getElementById('tagBusan').style.backgroundColor= "#DCE3E8";
+    document.getElementById('tagSeoul').style.color= "#000";
+    document.getElementById('tagGyeonggi').style.color= "#000";
+    document.getElementById('tagBusan').style.color= "#000";
 }
 
-function loadBusan() {
-    document.getElementById('listElement').innerHTML = '';
-    Busan();
+function changeTag(selectedTag) {
+    let tagList = ['tagSeoul', 'tagGyeonggi', 'tagSokcho', 'tagBusan'];
+    tagList = tagList.filter(tag => tag !== selectedTag)
+
+    document.getElementById(selectedTag).style.backgroundColor = "#186ADE";
+    document.getElementById(selectedTag).style.color = "#fff";
+
+    for (let tag of tagList) {
+        document.getElementById(tag).style.backgroundColor = "#DCE3E8";
+        document.getElementById(tag).style.color = "#000";
+    }
+  /*  document.getElementById('tagSeoul').style.backgroundColor= "#DCE3E8";
+    document.getElementById('tagGyeonggi').style.backgroundColor= "#DCE3E8";
+    document.getElementById('tagSokcho').style.backgroundColor= "#DCE3E8";
+    document.getElementById('tagSeoul').style.color= "#000";
+    document.getElementById('tagGyeonggi').style.color= "#000";
+    document.getElementById('tagSokcho').style.color= "#000";*/
 }
+
+
+
+
+
+
+/* html에서 onClick 사용해서 js에서 onClick 사용 안됨
+
+document.getElementById('tagBusan').onclick = function () {
+    this.style.backgroundColor = "#186ADE";
+    this.style.color = "#fff";
+    document.getElementById('tagSeoul').style.backgroundColor= "#DCE3E8";
+    document.getElementById('tagGyeonggi').style.backgroundColor= "#DCE3E8";
+    document.getElementById('tagSokcho').style.backgroundColor= "#DCE3E8";
+    document.getElementById('tagSeoul').style.color= "#000";
+    document.getElementById('tagGyeonggi').style.color= "#000";
+    document.getElementById('tagSokcho').style.color= "#000";
+}
+*/
