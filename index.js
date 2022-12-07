d5f15dd5fe104a0d66dbf780cac382f275d1706f
Jaewon Kim <macimaro04@gmail.com>
2022-12-07 15:34:03 +0900

    Design: 여행 리스트 ui변경 및 hover 효과 추가

--- index.js
+++ index.js
@@ -2,14 +2,25 @@ function getCityData(city) {
     fetch(`http://localhost:8000/data/${city}`) // 해당 주소의 api를 불러옴
         .then((response) => response.json()) // 불러온 api 값을 response로 받고 response값을 json으로 리턴 한다.
         .then((data) => { //그 값이 data라는 변수에 json array 형태로 들어감
+            let num = 1;
             data.forEach(element => { //가져온 값의 모든 값을 가져오기 위해 forEach문으로 모든 요소를 반복해 가져옴
                 const list = document.getElementById('listElement') // 읽어온 데이터를 추가할 리스트의 아이디를 가져옴
                 list.insertAdjacentHTML('beforeend', `
-                <li class = "auction-row" onclick = "window.open('https://www.mangoplate.com/search/${element.명소}')">
+                <div class="restaurant-wrapper" onclick = "window.open('https://www.mangoplate.com/search/${element.명소}')">
+                    <p class="find-restaurant">'${element.명소}' 주변 맛집 확인하기→</p>
+                </div>
+                <li class = "auction-row">
+                    <div class="list-number-wrapper">
+                        <div class="list-number">${num}</div>
+                    </div>
+                    <div class="auction-container">
                         <p class = "auctionID">${element.명소}</p>
                         <p class = "auctionAddress">⭐${element.별점}</p>
-                        </li>
+                    </div>
+                </li>
+                <div class="solid-line"></div>
                 `); // 아이디로 가져온 값을 html 문으로 반복해서 뒤에 추가함
+                num++;  // 순위 증가
             });
         });
 
