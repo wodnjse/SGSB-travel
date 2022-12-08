0a03dc1a0c8d90fb8c96bea9be31504477da2432
Jeromy0515 <qkr07224@gmail.com>
2022-12-08 00:41:01 +0000

    intergrate file path using path parameter

--- api.py
+++ api.py
@@ -3,22 +3,7 @@ import json
 
 app = FastAPI()
 
-@app.get('/data/seoul')
-def get_d():
-    with open('./json/seoul.json','r',encoding='utf-8') as f:
-        return json.loads(f.read())
-
-@app.get('/data/gyeonggi')
-def get_d():
-    with open('./json/gyeonggi.json','r',encoding='utf-8') as f:
-        return json.loads(f.read())
-
-@app.get('/data/sokcho')
-def get_d():
-    with open('./json/sokcho.json','r',encoding='utf-8') as f:
-        return json.loads(f.read())
-
-@app.get('/data/busan')
-def get_d():
-    with open('./json/busan.json','r',encoding='utf-8') as f:
+@app.get('/data/{city}')
+def get_d(city):
+    with open('./json/%s.json' % city, 'r', encoding='utf-8') as f:
         return json.loads(f.read())
